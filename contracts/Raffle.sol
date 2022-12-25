77a3bfdb45429dc36567ac0b934fce9504bfd449
Mamitiana <solofomamitiana0@gmail.com>
2022-12-25 11:58:13 +0300

    Pre (14:50:38) Code Cleanup; didn_t try to compile yet

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -8,6 +8,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 error Raffle__NotEnoughETHEntered();
 error Raffle__TransferFailed();
 error Raffle__NotOpen();
+error Raffle__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
 
 // Raffle
 contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
@@ -77,7 +78,7 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
   function checkUpkeep(
     bytes calldata /* checkData */
   )
-    external
+    public
     override
     returns (
       bool upkeepNeeded,
@@ -91,10 +92,20 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
     upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
   }
 
-  function requestRandomWinner() external {
+  function performUpkeep(
+    bytes calldata /* performData */
+  ) external override {
     // Request a random number
     // Once we get it, do something with it
     // 2 transaction process
+    (bool upkeepNeeded, ) = checkUpkeep("");
+    if (!upkeepNeeded) {
+      revert Raffle__UpkeepNotNeeded(
+        address(this).balance,
+        s_players.length,
+        uint256(s_raffleState)
+      );
+    }
     s_raffleState = RaffleState.CALCULATING;
     uint256 requestId = i_vrfCoordinator.requestRandomWords(
       i_gasLane,
@@ -115,6 +126,7 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
     s_recentWinner = recentWinner;
     s_raffleState = RaffleState.OPEN;
     s_players = new address payable[](0);
+    s_lastTimeStamp = block.timestamp;
     (bool success, ) = recentWinner.call{value: address(this).balance}("");
     if (!success) {
       revert Raffle__TransferFailed();
