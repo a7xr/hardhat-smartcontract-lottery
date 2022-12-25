abbc133623201a238b7eb52b0bbdfa58d5b71c6c
Mamitiana <solofomamitiana0@gmail.com>
2022-12-25 09:55:02 +0300

    Pre l9_(14:14:32) Hardhat Shorthand

---
+++ Commit_README.md
@@ -0,0 +1,1 @@
+Going to do l9_(14:14:32) Hardhat Shorthand
--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 error Raffle__NotEnoughETHEntered();
 
 // Raffle
-contract Raffle {
-  uint256 private i_entranceFee;
+contract Raffle is VRFConsumerBaseV2 {
+  uint256 private immutable i_entranceFee;
   address payable[] private s_players;
 
-  constructor(uint256 entranceFee) {
+  event RaffleEnter(address indexed player);
+
+  constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
     i_entranceFee = entranceFee;
   }
 
@@ -18,8 +23,20 @@ contract Raffle {
   function enterRaffle() public payable {
     if (msg.value < i_entranceFee) {
       revert Raffle__NotEnoughETHEntered();
+      // return true;
     }
     s_players.push(payable(msg.sender));
+    emit RaffleEnter(msg.sender);
+  }
+
+  function requestRandomWinner() external {
+    // Request a random number
+    // Once we get it, do something with it
+    // 2 transaction process
+  }
+
+  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+    //   console.log("")
   }
 
   function getPlayer(uint256 index) public view returns (address) {
