286111dadb359c164fc5769c27ee5bda7bf937dc
Mahyar <105216440+notmhyr@users.noreply.github.com>
2022-12-27 01:22:33 +0330

    Update 01-deployer-raffle.js
    i changed last one by mistake this one should solve your problem

--- deploy/01-deployer-raffle.js
+++ deploy/01-deployer-raffle.js
@@ -19,7 +19,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     subscriptionId = transactionReceipt.events[0].args.subId
     await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
   } else {
-    vrfCoordinatorV2Address = networkConfig[chainId]["VRFCoordinatorV2"]
+    vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
     subscriptionId = networkConfig[chainId]["subscriptionId"]
   }
 
