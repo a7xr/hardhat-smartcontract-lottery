0465bd2d74ff0cda9059ed1587bed39cea11dde0
Mamitiana <solofomamitiana0@gmail.com>
2022-12-26 13:09:21 +0300

    Pre 15:10:46 Deploying Raffle.sol Continued, did NOT compile yet

--- deploy/01-deployer-raffle.js
+++ deploy/01-deployer-raffle.js
@@ -1,9 +1,18 @@
+const { ethers } = require("ethers")
 const { networks } = require("../hardhat.config")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
+  let vrfCoordinatorV2Address 
 
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+  }
+
+  const args = []
   const raffle = await deploy("Raffle", {
     from: deployer,
     args: [],
